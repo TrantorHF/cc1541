f220dbdda2072b6200b088fc18af08a06e18e11d
Claus <claus@protovision.games>
2022-02-02 11:16:17 +0100

    Better handling of filetypes for transwarp: allows type to be set by user, but defaults to prg/usr as default. Allows open and protected flags in both cases.

---
+++ Xcode/cc1541/dummy/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  dummy
+//
+//  Created by Spenger, Claus-Christian on 01.02.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
